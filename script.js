ba1b8211024df5918032a9d804d20df230f4145b
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-11 23:41:08 +0300

    Timers & Intervals

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-setTimeout(function() {
-    console.log('Finished!');    
-}, 2000);
+    //Timers & Intervals
+// setTimeout(function() {
+//     console.log('Finished!');    
+// }, 2000);
+
+// setInterval(function() {
+//     console.log('Ping!');
+// }, 500);
+
+// var interval = setInterval(function() {
+//     console.log('Ping!');
+// }, 500);
+// setTimeout(function() {
+//     clearInterval(interval);
+// }, 2500);
