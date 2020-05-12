e9bad8c2e924d5e98342a477de1107f7f2698630
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-12 14:07:41 +0300

    The Math Object

--- script.js
+++ script.js
@@ -53,3 +53,25 @@
 // var string = 'Any text';
 // console.log(string.trim());
 
+
+
+    //The Math Object
+// var pi = Math.PI;
+// console.log(pi);
+
+// var e = Math.E;
+// console.log(e);
+
+// var a = -3;
+// console.log(Math.abs(a));
+
+// var a = 1.27;
+// console.log(Math.round(a));
+
+// var a = 1.27;
+// console.log(Math.ceil(a));
+
+// var a = 2;
+// console.log(Math.exp(a));
+
+
