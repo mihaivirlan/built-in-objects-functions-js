a4650e074e339ddbb36f781b82cac99fa8c89e0f
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-12 13:09:44 +0300

    Transforming Formats & Values

--- script.js
+++ script.js
@@ -13,3 +13,18 @@
 // setTimeout(function() {
 //     clearInterval(interval);
 // }, 2500);
+
+
+
+    //Transforming Formats & Values
+// var a = 's';
+// console.log(a);
+
+// var a = 'FBB123';
+// console.log(parseInt(a, 16));
+
+// var a = 10;
+// console.log(a.toString());
+
+var a = 10.3;
+console.log(a.toFixed());
