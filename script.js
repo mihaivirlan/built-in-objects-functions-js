b3a7197758e05a6c95c36fe1307fbbb8dc057f67
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-12 13:17:40 +0300

    String Functions

--- script.js
+++ script.js
@@ -26,5 +26,30 @@
 // var a = 10;
 // console.log(a.toString());
 
-var a = 10.3;
-console.log(a.toFixed());
+// var a = 10.3;
+// console.log(a.toFixed());
+
+
+
+    //String Functions
+// var string = 'Any text';
+// console.log(string[2]);
+
+// var string = 'Any text';
+// console.log(string.charAt(2));
+
+// var string = 'Any text';
+// console.log(string.concat(' add a new string'));
+
+// var string = 'Any text';
+// console.log(string.toUpperCase());
+
+// var string = 'Any text';
+// console.log(string.split(' '));
+
+// var string = 'Any text';
+// console.log(string);
+
+// var string = 'Any text';
+// console.log(string.trim());
+
